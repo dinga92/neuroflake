9478aa6f76876a945127d36f3878d132f6b0ccc9
work <dinga92@gmail.com>
2025-05-10 21:20:40 +0200

    change newnifti to make .so links same as in conda build

--- fsl-newnifti.nix
+++ fsl-newnifti.nix
@@ -12,40 +12,40 @@ in
       sha256 = "sha256-R4g+mFnmzcxTMGiKThiGxANi9ecxhY8uEQ+TKdkgKC0=";
     };
 
-    nativeBuildInputs = [pkgs.gcc pkgs.coreutils];
-
     buildInputs = [
       fslBase
       fslZnzlib
     ];
 
+    configurePhase = ''
+      export FSLDIR=${fslBase}
+      export FSLDEVDIR=${fslBase}
+      ln -sfn ${fslBase}/config config
+      source ${fslBase}/etc/fslconf/fsl-devel.sh
+    '';
+
     buildPhase = ''
-      make CXXFLAGS="$CXXFLAGS" LDFLAGS="$LDFLAGS" \
-           FSLSWDIR=${fslBase} \
-           FSLCONFDIR=${fslBase}/config
+      unset NIX_LDFLAGS
+      make \
+        ARCHLDFLAGS= \
+        ARCHLIBS= \
+        CXXFLAGS="$NIX_CXXFLAGS_COMPILE" \
+        LDFLAGS="-L${fslZnzlib}/lib -lfsl-znz -ldl -pthread" \
+        FSLCONFDIR=${fslBase}/config
     '';
 
     installPhase = ''
-
-      mkdir -p $out/etc
-
-      export FSLDIR=$out
-      export FSLDEVDIR=$out
-      export FSLCONFDIR=${fslBase}/config
-      # export PATH=${pkgs.coreutils}/bin:$PATH
-
-      # Verify paths before installation
-      echo "FSLDIR contents:"
-      ls -l $FSLDIR/etc
-
+      mkdir -p $out/{bin,lib,include,config,etc}
       make install \
-           FSLSWDIR=${fslBase} \
-           FSLCONFDIR=${fslBase}/config
+        verbose=v \
+        PREFIX=$out \
+        FSLDIR=$out \
+        FSLDEVDIR=$out \
+        FSLCONFDIR=${fslBase}/config
     '';
 
     meta = with pkgs.lib; {
       description = "FSL Utilities";
       homepage = "https://git.fmrib.ox.ac.uk/fsl/utils.git";
-      license = licenses.gpl3;
     };
   }
