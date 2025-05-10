3c5417220e595e4943a0aea0c4602eaac44096b0
work <dinga92@gmail.com>
2025-05-10 15:52:35 +0200

    change fsl-znzlib to match conda build

--- fsl-znzlib.nix
+++ fsl-znzlib.nix
@@ -1,3 +1,53 @@
+# {pkgs ? import <nixpkgs> {}}: let
+#   fslBase = pkgs.callPackage ./fsl-base.nix {};
+# in
+#   pkgs.stdenv.mkDerivation rec {
+#     pname = "fsl-znzlib";
+#     version = "2111.0";
+#
+#     src = pkgs.fetchgit {
+#       url = "https://git.fmrib.ox.ac.uk/fsl/znzlib.git";
+#       rev = version;
+#       sha256 = "sha256-cTRxxf27pt6eBejbSdiLeIP2nk/P/xCcxtUUvG4OT+Y=";
+#     };
+#
+#     nativeBuildInputs = [pkgs.gcc pkgs.coreutils];
+#     buildInputs = [fslBase pkgs.zlib];
+#
+#     buildPhase = ''
+#       make CXXFLAGS="$CXXFLAGS" LDFLAGS="$LDFLAGS" \
+#            FSLSWDIR=${fslBase} \
+#            FSLCONFDIR=${fslBase}/config
+#     '';
+#
+#     installPhase = ''
+#
+#       mkdir -p $out/etc
+#
+#       export FSLDIR=$out
+#       export FSLDEVDIR=$out
+#       export FSLCONFDIR=${fslBase}/config
+#       # export PATH=${pkgs.coreutils}/bin:$PATH
+#
+#       # Verify paths before installation
+#       echo "FSLDIR contents:"
+#       ls -l $FSLDIR/etc
+#
+#       make install \
+#            FSLSWDIR=${fslBase} \
+#            FSLCONFDIR=${fslBase}/config
+#
+#       mkdir -p $out/include
+#       ln -s ${pkgs.zlib.dev}/include/zlib.h $out/include/zlib.h
+#
+#     '';
+#
+#     meta = with pkgs.lib; {
+#       description = "FSL Utilities";
+#       homepage = "https://git.fmrib.ox.ac.uk/fsl/utils.git";
+#       license = licenses.gpl3;
+#     };
+#   }
 {pkgs ? import <nixpkgs> {}}: let
   fslBase = pkgs.callPackage ./fsl-base.nix {};
 in
@@ -11,40 +61,51 @@ in
       sha256 = "sha256-cTRxxf27pt6eBejbSdiLeIP2nk/P/xCcxtUUvG4OT+Y=";
     };
 
-    nativeBuildInputs = [pkgs.gcc pkgs.coreutils];
+    nativeBuildInputs = [pkgs.gcc];
     buildInputs = [fslBase pkgs.zlib];
 
+    configurePhase = ''
+      # Set up FSL environment variables
+      export FSLDIR=${fslBase}
+      export FSLDEVDIR=${fslBase}
+
+      # Link configuration directory
+      ln -sfn ${fslBase}/config config
+
+      # Source FSL development environment
+      source ${fslBase}/etc/fslconf/fsl-devel.sh
+    '';
+
     buildPhase = ''
-      make CXXFLAGS="$CXXFLAGS" LDFLAGS="$LDFLAGS" \
-           FSLSWDIR=${fslBase} \
-           FSLCONFDIR=${fslBase}/config
+      # Build with proper compiler flags
+
+      make \
+        CFLAGS="-I${pkgs.zlib.dev}/include -fPIC"\
+        LDFLAGS="-L${pkgs.zlib}/lib -lz" \
+        FSLCONFDIR=${fslBase}/config
     '';
 
     installPhase = ''
+      # Create output directories
+      mkdir -p $out/{lib,include}
 
-      mkdir -p $out/etc
+      # Install library manually
+      install -Dm755 libfsl-znz.so $out/lib/
 
-      export FSLDIR=$out
-      export FSLDEVDIR=$out
-      export FSLCONFDIR=${fslBase}/config
-      # export PATH=${pkgs.coreutils}/bin:$PATH
+      # Install headers
+      install -Dm644 znzlib.h $out/include/
 
-      # Verify paths before installation
-      echo "FSLDIR contents:"
-      ls -l $FSLDIR/etc
+      # Link zlib header if needed
+      ln -sf ${pkgs.zlib.dev}/include/zlib.h $out/include/zlib.h
 
-      make install \
-           FSLSWDIR=${fslBase} \
-           FSLCONFDIR=${fslBase}/config
-
-      mkdir -p $out/include
-      ln -s ${pkgs.zlib.dev}/include/zlib.h $out/include/zlib.h
-
+      mkdir -p $out/include/znzlib
+      install -Dm644 znzlib.h $out/include/znzlib/
+      install -Dm755 libfsl-znz.so $out/lib/
     '';
 
     meta = with pkgs.lib; {
-      description = "FSL Utilities";
-      homepage = "https://git.fmrib.ox.ac.uk/fsl/utils.git";
+      description = "FSL znzlib - NIfTI I/O library";
+      homepage = "https://git.fmrib.ox.ac.uk/fsl/znzlib";
       license = licenses.gpl3;
     };
   }
