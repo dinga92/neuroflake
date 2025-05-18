900fbdfe59c6e69318a6bda00fc2802e6fa7d3e3
work <dinga92@gmail.com>
2025-05-18 23:33:42 +0200

    fix fast 4 build

--- fsl-fast4.nix
+++ fsl-fast4.nix
@@ -1,96 +1,3 @@
-# {pkgs ? import <nixpkgs> {}}: let
-#   fslBase = pkgs.callPackage ./fsl-base.nix {};
-#   fslArmawrap = pkgs.callPackage ./fsl-armawrap.nix {};
-#   fslMiscmaths = pkgs.callPackage ./fsl-miscmaths.nix {};
-#   fslMiscTcl = pkgs.callPackage ./fsl-misc_tcl.nix {};
-#   fslNewimage = pkgs.callPackage ./fsl-newimage.nix {};
-#   fslNewnifti = pkgs.callPackage ./fsl-newnifti.nix {};
-#   fslUtils = pkgs.callPackage ./fsl-utils.nix {};
-#   fslZnzlib = pkgs.callPackage ./fsl-znzlib.nix {};
-#   fslCprob = pkgs.callPackage ./fsl-cprob.nix {};
-#
-#   openblas = pkgs.openblas;
-#   lapack = pkgs.lapack;
-#   blas = pkgs.blas;
-# in
-#   pkgs.stdenv.mkDerivation rec {
-#     pname = "fsl-fast4";
-#     version = "2111.3";
-#
-#     src = pkgs.fetchgit {
-#       url = "https://git.fmrib.ox.ac.uk/fsl/fast4.git";
-#       rev = "2111.3";
-#       sha256 = "sha256-pvurhxjbvKShikJyL0lC3N2aXVln61vG0EGZ0IRfHig=";
-#     };
-#
-#     buildInputs = [
-#       fslBase
-#       fslArmawrap
-#       fslMiscmaths
-#       fslMiscTcl
-#       fslNewimage
-#       fslNewnifti
-#       fslUtils
-#       fslZnzlib
-#       fslCprob
-#       pkgs.lapack
-#       pkgs.blas
-#       pkgs.libz
-#     ];
-#
-#     configurePhase = ''
-#       export FSLDIR=${fslBase}
-#       export FSLDEVDIR=${fslBase}
-#       ln -sfn ${fslBase}/config config
-#       source ${fslBase}/etc/fslconf/fsl-devel.sh
-#     '';
-#
-#     buildPhase = ''
-#       make \
-#         FSLSWDIR=${fslBase} \
-#         FSLCONFDIR=${fslBase}/config
-#     '';
-#
-#     installPhase = ''
-#       mkdir -p $out/etc
-#
-#       export FSLDIR=$out
-#       export FSLDEVDIR=$out
-#       export FSLCONFDIR=${fslBase}/config
-#
-#       make install \
-#            FSLSWDIR=${fslBase} \
-#            FSLCONFDIR=${fslBase}/config
-#     '';
-#
-#     # buildPhase = ''
-#     #   export FSLDIR=$out
-#     #   export FSLDEVDIR=$out
-#     #
-#     #   # Ensure FSL configurations are in place
-#     #   mkdir -p $out/config
-#     #   ln -s ${fslBase}/config/* $out/config/.
-#     #
-#     #   mkdir -p $out/etc/fslconf
-#     #   ln -s ${fslBase}/etc/fslconf/fsl.sh $out/etc/fslconf/fsl.sh
-#     #
-#     #   source ${fslBase}/etc/fslconf/fsl-devel.sh
-#     #
-#     #   # Add LAPACK and BLAS to LDFLAGS
-#     #   export LDFLAGS="-L${pkgs.lapack}/lib -L${pkgs.blas}/lib $LDFLAGS"
-#     #
-#     #   mkdir -p $out/include
-#     #   ln -s ${fslArmawrap}/include/* $out/include/
-#     #
-#     #   # Run the build (make)
-#     #   make
-#     # '';
-#
-#     meta = {
-#       description = "FSL FAST4 module";
-#       homepage = "https://git.fmrib.ox.ac.uk/fsl/fast4.git";
-#     };
-#   }
 {pkgs ? import <nixpkgs> {}}: let
   fslBase = pkgs.callPackage ./fsl-base.nix {};
   fslArmawrap = pkgs.callPackage ./fsl-armawrap.nix {};
@@ -101,6 +8,10 @@
   fslUtils = pkgs.callPackage ./fsl-utils.nix {};
   fslZnzlib = pkgs.callPackage ./fsl-znzlib.nix {};
   fslCprob = pkgs.callPackage ./fsl-cprob.nix {};
+
+  openblas = pkgs.openblas;
+  lapack = pkgs.lapack;
+  blas = pkgs.blas;
 in
   pkgs.stdenv.mkDerivation rec {
     pname = "fsl-fast4";
@@ -108,13 +19,13 @@ in
 
     src = pkgs.fetchgit {
       url = "https://git.fmrib.ox.ac.uk/fsl/fast4.git";
-      rev = version;
+      rev = "2111.3";
       sha256 = "sha256-pvurhxjbvKShikJyL0lC3N2aXVln61vG0EGZ0IRfHig=";
     };
 
     buildInputs = [
       fslBase
-      fslArmawrap # ← makes libfsl-armawrap.so available
+      fslArmawrap
       fslMiscmaths
       fslMiscTcl
       fslNewimage
@@ -122,43 +33,61 @@ in
       fslUtils
       fslZnzlib
       fslCprob
-      pkgs.blas
       pkgs.lapack
-      pkgs.gfortran
-      pkgs.gfortran.cc
-      pkgs.zlib
+      pkgs.blas
+      pkgs.libz
     ];
 
     configurePhase = ''
-      export FSLDIR=$PWD
-      export FSLDEVDIR=$PWD
-
-      mkdir -p etc/fslconf config include lib
-      ln -s ${fslBase}/config/default.mk etc/fslconf/default.mk
-      ln -s ${fslBase}/config/rules.mk   etc/fslconf/rules.mk
-      ln -s ${fslBase}/etc/fslconf/*.sh  etc/fslconf/
-      export FSLCONFDIR=$PWD/etc/fslconf
-      source $FSLCONFDIR/fsl-devel.sh
-
-      ln -s ${fslMiscmaths}/include/miscmaths include/
-      ln -s ${fslMiscmaths}/lib/libmiscmaths.*  lib/
+      export FSLDIR=${fslBase}
+      export FSLDEVDIR=${fslBase}
+      ln -sfn ${fslBase}/config config
+      source ${fslBase}/etc/fslconf/fsl-devel.sh
     '';
 
     buildPhase = ''
-      export FSLDIR=$PWD
-      export FSLCONFDIR=$PWD/etc/fslconf
-      make FSLDEVDIR=$PWD FSLSWDIR=$PWD FSLCONFDIR=$FSLCONFDIR
+      make \
+        FSLSWDIR=${fslBase} \
+        FSLCONFDIR=${fslBase}/config
     '';
 
     installPhase = ''
-      export FSLDIR=$PWD
-      export FSLCONFDIR=$PWD/etc/fslconf
-      make install FSLDEVDIR=$PWD FSLSWDIR=$PWD FSLCONFDIR=$FSLCONFDIR
-      cp -r . $out
+      mkdir -p $out/etc
+
+      export FSLDIR=$out
+      export FSLDEVDIR=$out
+      export FSLCONFDIR=${fslBase}/config
+
+      make install \
+           FSLSWDIR=${fslBase} \
+           FSLCONFDIR=${fslBase}/config
     '';
 
-    meta = with pkgs.lib; {
-      description = "FSL FAST (version 4)";
-      homepage = "https://git.fmrib.ox.ac.uk/fsl/fast4";
+    # buildPhase = ''
+    #   export FSLDIR=$out
+    #   export FSLDEVDIR=$out
+    #
+    #   # Ensure FSL configurations are in place
+    #   mkdir -p $out/config
+    #   ln -s ${fslBase}/config/* $out/config/.
+    #
+    #   mkdir -p $out/etc/fslconf
+    #   ln -s ${fslBase}/etc/fslconf/fsl.sh $out/etc/fslconf/fsl.sh
+    #
+    #   source ${fslBase}/etc/fslconf/fsl-devel.sh
+    #
+    #   # Add LAPACK and BLAS to LDFLAGS
+    #   export LDFLAGS="-L${pkgs.lapack}/lib -L${pkgs.blas}/lib $LDFLAGS"
+    #
+    #   mkdir -p $out/include
+    #   ln -s ${fslArmawrap}/include/* $out/include/
+    #
+    #   # Run the build (make)
+    #   make
+    # '';
+
+    meta = {
+      description = "FSL FAST4 module";
+      homepage = "https://git.fmrib.ox.ac.uk/fsl/fast4.git";
     };
   }
